c9ee2779a471aea352351fc1b11890b7d8a2e466
tranhiep266 <hiep5apy@gmail.com>
2024-11-24 21:26:17 +0700

    Add files via upload

--- Session4_Ex1.cpp
+++ Session4_Ex1.cpp
@@ -5,6 +5,8 @@ int main(){
 	scanf("%d", &number);
 	if(number > 0){
 		printf("%d la so nguyen duong", number);
+	}else if(number == 0){
+		printf("%d khong la so nguyen duong, khong la so nguyen am", number);
 	}
 	else{
 		printf("%d la so nguyen am", number);
--- Session4_Ex9.cpp
+++ Session4_Ex9.cpp
@@ -11,14 +11,20 @@ int main() {
     if (month < 1 || month > 12) {
         printf("Ngay %d/%d/%d khong hop le \n", day, month, year);
     } else {
-        // Ki?m tra ngày
+    	int isLeap = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
         if (day < 1) {
             printf("Ngay %d/%d/%d khong hop le \n", day, month, year);
         } else {
             if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
                 printf("Ngay %d/%d/%d khong hop le \n", day, month, year);
-            } else if (month == 2 && day > 28) {
+            } else if (month == 2 ) {
+            	if (isLeap && day>29){
+            		printf("Ngay %d/%d/%d khong hop le \n", day, month, year);
+				}else if(!isLeap && day > 28){
                 printf("Ngay %d/%d/%d khong hop le \n", day, month, year);
+            }else{
+            	printf("Ngay %d/%d/%d là hop le \n", day, month, year);
+			}
             } else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 31) {
                 printf("Ngay %d/%d/%d khong hop le \n", day, month, year);
             } else {
